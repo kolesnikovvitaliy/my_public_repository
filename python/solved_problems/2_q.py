15aff0ba92115dca0dbfbe9b3d61651279f626ec
Виталий Колесников <kolesnikovvitaliy@mail.ru>
2022-12-23 11:05:13 +0300

    red 2 files

--- python/solved_problems/2_q.py
+++ python/solved_problems/2_q.py
@@ -1,1 +1,12 @@
-print([i for i in input() if i in '1234567890'])
+def print_digit_sum(num):
+    c = 0
+    while num > 0:
+        c += num % 10
+        num //= 10
+    print(c)
+
+# считываем данные
+n = int(input())
+
+# вызываем функцию
+print_digit_sum(n)
